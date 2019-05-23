0979b92796e3424e0d5da0fd9acdb04532818004
edgon <edgon85@gmail.com>
2019-05-23 11:46:19 -0600

    Disenio basico - parte 1

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-# disenio
+# Diseño de apps en flutter
 
 Diseño de app en Flutter
 
+[1 - Incio de proyecto Diseños](../../tree/207f5fcb3f93c6b7fa20d637371bc9eb6cb586b9/)
 
--- lib/src/basic_page.dart
+++ lib/src/basic_page.dart
@@ -1,11 +1,39 @@
 import 'package:flutter/material.dart';
 
 class BasicPage extends StatelessWidget {
+
+  final styleTitle = TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold);
+  final styleSubTitle = TextStyle(fontSize: 16.0, fontWeight: FontWeight.normal);
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Basico page'),
+      body: Column(
+        children: <Widget>[
+          Image(image: NetworkImage('https://i.pinimg.com/originals/ca/68/8d/ca688dcdd1218f6ca5b1af6a355211c3.jpg'),),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+            child: Row(
+              children: <Widget>[
+
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text('Carreterra en montaña', style: styleTitle,),
+                      SizedBox(height: 7.0,),
+                      Text('Guatemala, aaahh no cierto', style: styleSubTitle,)
+                    ],
+                  ),
+                ),
+
+                Icon(Icons.star, color: Colors.red, size: 30.0,),
+                Text('41', style: TextStyle(fontSize: 20.0),)
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
